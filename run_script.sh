d3e6c85b38f78d6aec6563e15d375d955095e6fd
Kislay Kishore <kislayk@google.com>
2025-01-31 16:52:59 +0530

    Add run script

---
+++ run_script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http

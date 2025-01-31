9033a0718352dfc5a8b41b7ad54527b38f1780ce
Kislay Kishore <kislayk@google.com>
2025-01-31 16:53:59 +0530

    Change bucket name

--- run_script.sh
+++ run_script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol grpc
-go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_fio_bucket --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol http
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol grpc
+go run . --warm-up-time 2m --run-time 5m --worker 128 --bucket kislayk_europe_west4 --client-protocol http

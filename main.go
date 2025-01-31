edf2452046d75898e8ac512d84975739b6781973
Kislay Kishore <kislayk@google.com>
2025-01-31 16:30:55 +0530

    Upgrade go storage

--- main.go
+++ main.go
@@ -6,17 +6,12 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
-	"sync/atomic"
-
-	// Register the pprof endpoints under the web server root at /debug/pprof
-	_ "net/http/pprof"
 	"os"
 	"strconv"
+	"sync/atomic"
 	"time"
 
-	"cloud.google.com/go/profiler"
 	"cloud.google.com/go/storage"
 	"cloud.google.com/go/storage/experimental"
 	"github.com/googleapis/gax-go/v2"
@@ -191,38 +186,6 @@ func ReadObject(ctx context.Context, workerID int, bucketHandle *storage.BucketH
 func main() {
 	flag.Parse()
 	ctx := context.Background()
-
-	if *enableTracing {
-		cleanup := enableTraceExport(ctx, *traceSampleRate)
-		defer cleanup()
-	}
-
-	// Start a pprof server.
-	// Example usage (run the following command while the script is running):
-	// go tool pprof http://localhost:8080/debug/pprof/profile?seconds=60
-	if *enablePprof {
-		go func() {
-			if err := http.ListenAndServe("localhost:8080", nil); err != nil {
-				log.Fatalf("error starting http server for pprof: %v", err)
-			}
-		}()
-	}
-
-	if *enableCloudProfiler {
-		if err := profiler.Start(profiler.Config{
-			Service:              "custom-go-benchmark",
-			ServiceVersion:       *version,
-			ProjectID:            *projectID,
-			NoCPUProfiling:       !*enableCPU,
-			NoHeapProfiling:      !*enableHeap,
-			NoAllocProfiling:     !*enableHeapAlloc,
-			NoGoroutineProfiling: !*enableThread,
-			MutexProfiling:       *enableContention,
-			DebugLogging:         true,
-		}); err != nil {
-			log.Fatalf("Failed to start profiler: %v", err)
-		}
-	}
 
 	var client *storage.Client
 	var err error

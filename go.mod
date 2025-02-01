460561ce9b1928a081680586a997046f214e8b83
Kislay Kishore <kislayk@google.com>
2025-02-01 15:24:56 +0530

    Minor fixes

--- go.mod
+++ go.mod
@@ -4,14 +4,10 @@ go 1.22.9
 
 require (
 	cloud.google.com/go/storage v1.50.0
-	contrib.go.opencensus.io/exporter/stackdriver v0.13.14
-	github.com/google/go-cmp v0.6.0
 	github.com/googleapis/gax-go/v2 v2.14.1
 	github.com/jacobsa/gcloud v0.0.0-20240721050538-15c4fa4a75e7
-	go.opencensus.io v0.24.0
 	golang.org/x/oauth2 v0.25.0
 	golang.org/x/sync v0.10.0
-	gonum.org/v1/plot v0.15.0
 	google.golang.org/api v0.217.0
 )
 
@@ -23,38 +19,26 @@ require (
 	cloud.google.com/go/compute/metadata v0.6.0 // indirect
 	cloud.google.com/go/iam v1.3.1 // indirect
 	cloud.google.com/go/monitoring v1.22.1 // indirect
-	cloud.google.com/go/trace v1.11.3 // indirect
-	git.sr.ht/~sbinet/gg v0.6.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/detectors/gcp v1.25.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/metric v0.49.0 // indirect
 	github.com/GoogleCloudPlatform/opentelemetry-operations-go/internal/resourcemapping v0.49.0 // indirect
-	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
-	github.com/aws/aws-sdk-go v1.55.5 // indirect
-	github.com/campoy/embedmd v1.0.0 // indirect
-	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cncf/xds/go v0.0.0-20241223141626-cff3c89139a3 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/envoyproxy/go-control-plane/envoy v1.32.3 // indirect
 	github.com/envoyproxy/protoc-gen-validate v1.1.0 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-fonts/liberation v0.3.3 // indirect
-	github.com/go-latex/latex v0.0.0-20240709081214-31cef3c7570e // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/go-pdf/fpdf v0.9.0 // indirect
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
-	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/jacobsa/oglematchers v0.0.0-20150720000706-141901ea67cd // indirect
 	github.com/jacobsa/oglemock v0.0.0-20150831005832-e94d794d06ff // indirect
 	github.com/jacobsa/reqtrace v0.0.0-20150505043853-245c9e0234cb // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/planetscale/vtprotobuf v0.6.1-0.20240319094008-0393e58bdf10 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/prometheus/prometheus v0.301.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/detectors/gcp v1.34.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.58.0 // indirect
@@ -65,8 +49,6 @@ require (
 	go.opentelemetry.io/otel/sdk/metric v1.34.0 // indirect
 	go.opentelemetry.io/otel/trace v1.34.0 // indirect
 	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
-	golang.org/x/image v0.21.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
-	"cloud.google.com/go/storage/experimental"
 	"github.com/googleapis/gax-go/v2"
 	"golang.org/x/oauth2"
 	"golang.org/x/sync/errgroup"
@@ -25,10 +24,10 @@ var (
 	maxConnsPerHost     = 100
 	maxIdleConnsPerHost = 0
 
-	// MB means 1024 Kb.
+	// MiB means 1024 KiB.
 	MiB = int64(1024 * 1024)
 
-	numOfWorker = flag.Int("worker", 128, "Number of concurrent worker to read")
+	numOfWorkers = flag.Int("worker", 128, "Number of concurrent worker to read")
 
 	runTime = flag.Duration("run-time", 3*time.Minute, "Actual workload runtime")
 
@@ -40,54 +39,24 @@ var (
 
 	bucketName = flag.String("bucket", "kislayk_europe_west4", "GCS bucket name.")
 
-	// ProjectName denotes gcp project name.
-	ProjectName = flag.String("project", "tpu-prod-env-one-vm", "GCP project name.")
-
 	clientProtocol = flag.String("client-protocol", "http", "Network protocol.")
 
 	// Object name = objectNamePrefix + {thread_id} + objectNameSuffix
 	objectNamePrefix = flag.String("obj-prefix", "1GB/experiment.", "Object prefix")
 	objectNameSuffix = flag.String("obj-suffix", ".0", "Object suffix")
-
-	tracerName      = "kislayk-storage-benchmark"
-	enableTracing   = flag.Bool("enable-tracing", false, "Enable tracing with Cloud Trace export")
-	enablePprof     = flag.Bool("enable-pprof", false, "Enable pprof server")
-	traceSampleRate = flag.Float64("trace-sample-rate", 1.0, "Sampling rate for Cloud Trace")
-
-	// Cloud profiler.
-	enableCloudProfiler = flag.Bool("enable-cloud-profiler", false, "Enable cloud profiler")
-	enableHeap          = flag.Bool("heap", false, "enable heap profile collection")
-	enableCPU           = flag.Bool("cpu", true, "enable cpu profile collection")
-	enableHeapAlloc     = flag.Bool("heap_alloc", false, "enable heap allocation profile collection")
-	enableThread        = flag.Bool("thread", false, "enable thread profile collection")
-	enableContention    = flag.Bool("contention", false, "enable contention profile collection")
-	projectID           = flag.String("project_id", "", "project ID to run profiler with; only required when running outside of GCP.")
-	version             = flag.String("version", "original", "version to run profiler with")
-
-	// Enable read stall retry.
-	enableReadStallRetry = flag.Bool("enable-read-stall-retry", false, "Enable read stall retry")
 )
 
 // CreateHTTPClient create http storage client.
-func CreateHTTPClient(ctx context.Context, isHTTP2 bool) (client *storage.Client, err error) {
+func CreateHTTPClient(ctx context.Context) (client *storage.Client, err error) {
 	var transport *http.Transport
 	// Using http1 makes the client more performant.
-	if !isHTTP2 {
-		transport = &http.Transport{
-			MaxConnsPerHost:     maxConnsPerHost,
-			MaxIdleConnsPerHost: maxIdleConnsPerHost,
-			// This disables HTTP/2 in transport.
-			TLSNextProto: make(
-				map[string]func(string, *tls.Conn) http.RoundTripper,
-			),
-		}
-	} else {
-		// For http2, change in MaxConnsPerHost doesn't affect the performance.
-		transport = &http.Transport{
-			DisableKeepAlives: true,
-			MaxConnsPerHost:   maxConnsPerHost,
-			ForceAttemptHTTP2: true,
-		}
+	transport = &http.Transport{
+		MaxConnsPerHost:     maxConnsPerHost,
+		MaxIdleConnsPerHost: maxIdleConnsPerHost,
+		// This disables HTTP/2 in transport.
+		TLSNextProto: make(
+			map[string]func(string, *tls.Conn) http.RoundTripper,
+		),
 	}
 
 	tokenSource, err := GetTokenSource(ctx, "")
@@ -97,25 +66,14 @@ func CreateHTTPClient(ctx context.Context, isHTTP2 bool) (client *storage.Client
 
 	// Custom http client for Go Client.
 	httpClient := &http.Client{
-		Transport: &oauth2.Transport{
-			Base:   transport,
-			Source: tokenSource,
+		Transport: &userAgentRoundTripper{
+			wrapped: &oauth2.Transport{
+				Base:   transport,
+				Source: tokenSource,
+			},
+			UserAgent: "prince",
 		},
 		Timeout: 0,
-	}
-
-	// Setting UserAgent through RoundTripper middleware
-	httpClient.Transport = &userAgentRoundTripper{
-		wrapped:   httpClient.Transport,
-		UserAgent: "prince",
-	}
-
-	if *enableReadStallRetry {
-		return storage.NewClient(ctx, option.WithHTTPClient(httpClient),
-			experimental.WithReadStallTimeout(&experimental.ReadStallTimeoutConfig{
-				Min:              time.Second,
-				TargetPercentile: 0.99,
-			}))
 	}
 	return storage.NewClient(ctx, option.WithHTTPClient(httpClient))
 }
@@ -129,13 +87,12 @@ func rampUp(warmupCtx context.Context, cancelFn context.CancelFunc, bucketHandle
 			cancelFn()
 			return
 		default:
-			if idx == *numOfWorker {
+			if idx == *numOfWorkers {
 				fmt.Println("Waiting")
 				eG.Wait()
 				return
 			}
 			time.Sleep(1 * time.Second)
-			//fmt.Printf("Ramping up for idx: %d\n", idx)
 			eG.Go(func() error {
 				_, err := ReadObject(warmupCtx, idx, bucketHandle)
 				if err != nil {
@@ -193,7 +150,7 @@ func main() {
 	protocol := ""
 	if *clientProtocol == "http" {
 		protocol = "http"
-		client, err = CreateHTTPClient(ctx, false)
+		client, err = CreateHTTPClient(ctx)
 	} else {
 		protocol = "grpc"
 		client, err = CreateGrpcClient(ctx)
@@ -229,7 +186,7 @@ func main() {
 	defer cancelFn()
 
 	// Run the actual workload
-	for i := 0; i < *numOfWorker; i++ {
+	for i := 0; i < *numOfWorkers; i++ {
 		idx := i
 		eG.Go(func() error {
 			//fmt.Printf("Worker %d started\n", idx)
